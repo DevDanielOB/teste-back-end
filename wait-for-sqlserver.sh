ee19b3615f6cf75d016ee5761be0e06e5efa23e6
Daniel <oliveiradb1@gmail.com>
2025-07-30 15:59:01 -0300

    Feat: Ajustando infra

--- wait-for-sqlserver.sh
+++ wait-for-sqlserver.sh
@@ -1,6 +1,4 @@
-#!/bin/bash
 
-# Aguarda o SQL Server estar pronto
 until nc -z -v -w30 sqlserver 1433
 do
   echo "Aguardando o SQL Server estar pronto..."
@@ -9,5 +7,4 @@ done
 
 echo "SQL Server está pronto! Iniciando o NestJS..."
 
-# Inicia o NestJS
 exec "$@"
